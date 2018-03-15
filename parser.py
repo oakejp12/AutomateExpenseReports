a38c4ec66c66989d61d63965687c43c52fbc17e9
Johan Oakes <joakes@silasg.com>
2018-03-15 13:53:14 -0400

    Add class to parse emails and calculate total expenses

---
+++ parser.py
@@ -0,0 +1,57 @@
+'''
+    Parse the Outlook message bodies into
+    a readable format.
+'''
+
+from outlook import messageBodies
+import re
+from decimal import Decimal
+
+class Parse():
+    
+    def __init__(self, body):
+        self.body = body
+        self.lineFareSearch = r'(fare \(.*\))(.*)\$([0-9]+.[0-9]+)'
+        self.discountSearch = r'(Discount)(.*)\$([0-9]+.[0-9]+)'
+
+    def searchForFare(self, searchString):
+        '''
+            Search for the fare string in the message body:
+            Line fare
+            Lyft Line Discount 
+        '''
+        fareObj = re.search(searchString, self.body)
+        if fareObj:
+            lineFare = fareObj.group(3) 
+            return lineFare
+        else:
+            print("No fare found: " + searchString) # TODO: Throw an error here? Maybe not since discounts won't be found
+    
+    def getLineTotal(self):
+        '''
+            Calculate the total fare owed at the end of the ride
+        '''
+        lineFare = self.searchForFare(self.lineFareSearch)
+        discount = self.searchForFare(self.discountSearch)
+        if lineFare and discount is None:
+            return Decimal(lineFare)
+        elif lineFare and discount:
+            return Decimal(lineFare) - Decimal(discount)
+        else:
+            print("Couldn't retrieve values...")
+
+
+def parseMessages():
+    bodies = messageBodies()
+
+    totalsForEachMessage = [Parse(n).getLineTotal() for n in bodies]
+
+    [print(str(total)) for total in totalsForEachMessage]
+
+    print("Total " + str(sum(totalsForEachMessage)))
+
+
+if __name__ == "__main__":
+    parseMessages()
+
+    
